a1148ac0eaebf62bdaa2d698d53e14de2817650b
lichengxiang <cbiltps@yeah.net>
2025-04-23 10:24:06 +0800

    构建前端页面(使用题域矩阵项目修改)

---
+++ ai-tool-frontend/src/router/routes.ts
@@ -0,0 +1,94 @@
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "@/views/HomePage.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
+import ACCESS_ENUM from "@/access/accessEnum";
+import NoAuthPage from "@/views/NoAuthPage.vue";
+import UserLoginPage from "@/views/user/UserLoginPage.vue";
+import UserRegisterPage from "@/views/user/UserRegisterPage.vue";
+import AdminUserPage from "@/views/admin/AdminUserPage.vue";
+import AdminAppPage from "@/views/admin/AdminAppPage.vue";
+import AppDetailPage from "@/views/app/AppDetailPage.vue";
+import AddAppPage from "@/views/app/AddAppPage.vue";
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "主页",
+    component: HomePage,
+  },
+  {
+    path: "/add/app",
+    name: "创建应用",
+    component: AddAppPage,
+  },
+  {
+    path: "/add/app/:id",
+    name: "修改应用",
+    props: true,
+    component: AddAppPage,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/app/detail/:id",
+    name: "应用详情页",
+    props: true,
+    component: AppDetailPage,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/admin/user",
+    name: "用户管理",
+    component: AdminUserPage,
+    meta: {
+      access: ACCESS_ENUM.ADMIN,
+    },
+  },
+  {
+    path: "/admin/app",
+    name: "应用管理",
+    component: AdminAppPage,
+    meta: {
+      access: ACCESS_ENUM.ADMIN,
+    },
+  },
+  {
+    path: "/noAuth",
+    name: "无权限",
+    component: NoAuthPage,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/hide",
+    name: "隐藏页面",
+    component: HomePage,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginPage,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterPage,
+      },
+    ],
+    meta: {
+      hideInMenu: true,
+    },
+  },
+];
